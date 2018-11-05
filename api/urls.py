71e5426074455583c1c35a85177611551bd54448
Anthony Ngene <ngeneanthony@gmail.com>
2018-11-05 06:38:17 +0100

    (task): refactor api urls to be included in the cross_solar project

---
+++ api/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from rest_framework import routers
+
+from . import views
+
+router = routers.DefaultRouter()
+router.register('', views.PanelViewSet)
+
+urlpatterns = [
+    url(r'^(?P<panelid>\d+)/analytics/$', views.HourAnalyticsView.as_view()),
+    url(r'^(?P<panelid>\d+)/analytics/day/$', views.DayAnalyticsView.as_view()),
+]
+
+urlpatterns += router.urls
--- cross_solar/urls.py
+++ cross_solar/urls.py
@@ -1,16 +1,7 @@
-from django.urls import path, include
+from django.urls import include
 from django.conf.urls import url
-from rest_framework import routers
-from api import views
 
-router = routers.DefaultRouter()
-router.register('panel', views.PanelViewSet)
 
 urlpatterns = [
-    url(r'^panel/(?P<panelid>\d+)/analytics/$',
-        views.HourAnalyticsView.as_view()),
-    url(r'^panel/(?P<panelid>\d+)/analytics/day/$',
-        views.DayAnalyticsView.as_view()),
+    url(r'^panel/', include('api.urls')),
 ]
-
-urlpatterns += router.urls
