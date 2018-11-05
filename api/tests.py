6063f5e2e13870f97d7c1763fac4d272ed08e4fb
Anthony Ngene <ngeneanthony@gmail.com>
2018-11-05 19:02:05 +0100

    (feature): implement the DayAnalytics views and add relevant tests

--- api/tests.py
+++ api/tests.py
@@ -1,7 +1,6 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
-from .models import Panel
-
+from .models import Panel, OneHourElectricity
 
 class PanelTestCase(APITestCase):
     def setUp(self):
@@ -28,7 +27,7 @@ class HourAnalyticsTestCase(APITestCase):
         return self.client.post('/panel/1/analytics/', {
             "panel": 1,
             "kilo_watt": 200,
-            "date_time": "2018-11-04T03:33:00Z"
+            "date_time": "2018-11-04T03:00:00Z"
         }, format='json')
 
     def test_hourly_report_creation(self):
@@ -43,3 +42,55 @@ class HourAnalyticsTestCase(APITestCase):
         response = self.client.get('/panel/1/analytics/', format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertGreater(len(response.data), 0)
+
+
+class DayAnalyticsViewTestCase(APITestCase):
+    def setUp(self):
+        Panel.objects.create(brand="Areva", serial="AAAA1111BBBB2222",
+                             latitude=12.345678, longitude=98.765543)
+        data = [
+            {
+                "kilo_watt": 200,
+                "date_time": "2018-11-02T03:00:00Z"
+            },
+            {
+                "kilo_watt": 300,
+                "date_time": "2018-11-02T03:00:00Z"
+            },
+            {
+                "kilo_watt": 80,
+                "date_time": "2018-11-03T03:00:00Z"
+            },
+            {
+                "kilo_watt": 160,
+                "date_time": "2018-11-03T03:00:00Z"
+            },
+            {
+                "kilo_watt": 450,
+                "date_time": "2018-11-03T03:00:00Z"
+            }
+        ]
+        panel1 = Panel.objects.get(pk=1)
+        for item in data:
+            item['panel'] = panel1
+            OneHourElectricity.objects.create(**item)
+
+    def test_day_analytics_get(self):
+        response = self.client.get('/panel/1/analytics/day', format='json')
+        expected = [
+            {
+                "date_time": "2018-11-02",
+                "sum": 500,
+                "average": 250.0,
+                "maximum": 300,
+                "minimum": 200
+            },
+            {
+                "date_time": "2018-11-03",
+                "sum": 690,
+                "average": 230.0,
+                "maximum": 450,
+                "minimum": 80
+            }
+        ]
+        self.assertEquals(response.data, expected)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from rest_framework import viewsets, status
 from rest_framework.views import APIView, Response
 from .models import Panel, OneHourElectricity
@@ -29,11 +31,19 @@ class HourAnalyticsView(APIView):
 
 class DayAnalyticsView(APIView):
     def get(self, request, panelid):
-        # Please implement this method to return Panel's daily analytics data
-        return Response([{
-            "date_time": "[date for the day]",
-            "sum": 0,
-            "average": 0,
-            "maximum": 0,
-            "minimum": 0
-        }])
+        daily_report = defaultdict(list)
+        queryset = OneHourElectricity.objects.filter(panel_id=panelid)
+        for item in queryset:
+            daily_report[item.date_time.strftime('%Y-%m-%d')].append(item.kilo_watt)
+
+        result = []
+        for day, values in daily_report.items():
+            kilo_watt_sum = sum(values)
+            result.append({
+                'date_time': day,
+                'sum': kilo_watt_sum,
+                'average': kilo_watt_sum / len(values),
+                'maximum': max(values),
+                'minimum': min(values),
+            })
+        return Response(sorted(result, key=lambda x: x['date_time']))
